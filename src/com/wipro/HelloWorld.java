0b1f23c4b16b996121d2a28d77b6f45a746fec5a
dhiviyalakshmi <dhiviyalakshmi.v@gmail.com>
2017-09-03 17:10:11 +0530

    first

---
+++ src/com/wipro/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.wipro;
+import java.util.logging.Logger;
+
+public class HelloWorld {
+		private HelloWorld(){
+			
+		}
+		public static void main(String[] args)
+		{
+			Logger.getLogger("Hello");
+		}
+}
